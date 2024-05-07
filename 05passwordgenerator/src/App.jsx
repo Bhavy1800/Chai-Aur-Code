083d51820829530fe1c410331165b6c7cf7ae23e
Bhavy Patel <bhavyapatel1800@gmail.com>
2024-05-07 22:30:34 +0530

    update

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [symbolAllowed, setSymbolAllowed] = useState(false);
+  const [password, setPassword] = useState("");
   return (
     <>
       <h1 className="bg-red-400">Setup Ready</h1>
